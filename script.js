173d6b7baae78b551f14e9be4ecb02cd887fb9e8
savawedev <andrei.sava.dev@gmail.com>
2022-04-16 19:07:19 +0300

    Added function to add marker to the board

--- script.js
+++ script.js
@@ -8,15 +8,35 @@ const gameBoard = (() => {
     boardSection.classList.add("board");
     mainSection.appendChild(boardSection);
 
-    board.forEach((ele) => {
+    board.forEach((ele, i) => {
       const newBoardItem = document.createElement("div");
       newBoardItem.classList.add("board-item");
       newBoardItem.textContent = ele;
+      newBoardItem.setAttribute("data-position", i);
       boardSection.appendChild(newBoardItem);
     });
   };
 
-  return { board, renderBoard };
+  const addMarker = (marker) => {
+    const boardItems = document.querySelectorAll(".board-item");
+    boardItems.forEach((item) => {
+      item.addEventListener("click", (e) => {
+        boardPosition = e.target.getAttribute("data-position");
+        board[boardPosition] = marker;
+        e.target.textContent = marker;
+      });
+    });
+  };
+
+  return { board, renderBoard, addMarker };
 })();
 
+const Player = (name, marker) => {
+  return { name, marker };
+};
+
+const player1 = Player("player1", "X");
+const player2 = Player("player2", "0");
+
 gameBoard.renderBoard();
+gameBoard.addMarker(player1.marker);
